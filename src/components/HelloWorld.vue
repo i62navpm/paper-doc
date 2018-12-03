7f7db831b7d9deedd133d2b2fff56c85461d7162
m.navarro <m.navarro@payvision.com>
2018-12-03 22:01:08 +0100

    Format files

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,8 +2,8 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
+      For a guide and recipes on how to configure / customize this project,
+      <br />check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
         >vue-cli documentation</a
       >.
@@ -96,11 +96,14 @@
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: 'HelloWorld',
   props: {
-    msg: String
-  }
-};
+    msg: String,
+  },
+  data: function() {
+    return {}
+  },
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
