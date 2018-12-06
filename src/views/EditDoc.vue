05907103d7cacb79ca01aa522c2f36ab175651a6
m.navarro <m.navarro@payvision.com>
2018-12-06 18:50:18 +0100

    New edit doc view

---
+++ src/views/EditDoc.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="edit">
+    <h1>Edit your doc</h1>
+    <VSectionKey />
+  </div>
+</template>
+
+<script>
+import VSectionKey from '@/components/VSectionKey'
+
+export default {
+  name: 'EditDoc',
+  components: {
+    VSectionKey,
+  },
+}
+</script>
