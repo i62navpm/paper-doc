a5080c2816c2fee248c1bb686a0ef713fd34bc42
m.navarro <m.navarro@payvision.com>
2018-12-06 19:05:21 +0100

    Add sections func

--- src/views/EditDoc.vue
+++ src/views/EditDoc.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="edit">
     <h1>Edit your doc</h1>
-    <VSectionKey />
+    <VSectionKey
+      v-for="section in sections"
+      :key="section.id"
+      :section="section"
+    />
+
+    <div class="row flex-right">
+      <div class="col xs-4 align-middle">
+        <button
+          class="btn-block"
+          @click.prevent="addSection(++index)"
+        >
+          Add section <i class="fa fa-plus" />
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,6 +27,15 @@ export default {
   name: 'EditDoc',
   components: {
     VSectionKey,
+  },
+  data: () => ({
+    index: 1,
+    sections: [{ id: 1 }],
+  }),
+  methods: {
+    addSection(id) {
+      this.sections.push({ id })
+    },
   },
 }
 </script>
