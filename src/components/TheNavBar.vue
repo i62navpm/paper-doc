796b456837c0daae25dc7816b0b3ae3e4d03ee00
m.navarro <m.navarro@payvision.com>
2018-12-06 11:44:04 +0100

    The nav bar component

---
+++ src/components/TheNavBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav class="border fixed split-nav">
+    <div class="nav-brand">
+      <h3>
+        <a href="#">
+          Paper Doc
+        </a>
+      </h3>
+    </div>
+    <div class="collapsible">
+      <input
+        id="collapsible1"
+        type="checkbox"
+        name="collapsible1"
+      >
+      <button>
+        <label for="collapsible1">
+          <div class="bar1" />
+          <div class="bar2" />
+          <div class="bar3" />
+        </label>
+      </button>
+      <div class="collapsible-body">
+        <ul class="inline">
+          <li>
+            <a href="#">
+              Start
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'TheNavBar',
+}
+</script>
+
